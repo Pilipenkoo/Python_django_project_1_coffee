c2074fc8dc6194444d7f3a3c0012c218a8e9790f
Ivan Pilipenko <pilipenko437@gmail.com>
2024-05-11 20:18:37 +0300

    New upgrade

---
+++ cheremyxa/cheremyxa/__pycache__/a = 10.py
@@ -0,0 +1,17 @@
+﻿from abc import abstractclassmethod, ABC
+
+class Piece(ABC):
+    @abstractclassmethod
+    def move(self):
+        pass
+    
+class Queen(Piece):
+    def move(self):
+        print('Ход')
+        
+a = Piece()
+b =   Queen(
+)
+
+a.move()
+b.move()
